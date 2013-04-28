04555c733e5c782b33094d1b7577d590a355cc51
David Young <davidyoung604@gmail.com>
2013-04-27 22:25:50 -0700

    Bookmark folder creation working. Need to get ID of folder to add bookmarks to it.

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 var filter = "";
 var windowId;
 var window_div = document.getElementById("windows");
+var filteredTabs = null;
 
 function windowHeader(num, win_id) {
 	if (win_id == windowId) {
@@ -18,6 +19,7 @@ function printTabs(tab_array) {
 	for (tab_index = 0; tab_index < tab_array.length; tab_index++) {
 		var url = tab_array[tab_index].url;
 		if ( re.test(url) ) {
+			filteredTabs.push(tab_array[tab_index]);
 			window_div.innerHTML += url + "<br />";
 		}
 	}
@@ -38,6 +40,7 @@ function updateTabList() {
 	document.getElementById("windows").innerHTML = "";
 	filter = document.getElementById("filter").value;
 	chrome.windows.getCurrent(retrieveCurrentWindowId);
+	filteredTabs = [];
 	
 	if (document.getElementById("onlyCurrentWindow").checked) {
 		chrome.tabs.getAllInWindow(windowId, printTabs);
@@ -46,9 +49,40 @@ function updateTabList() {
 	}
 }
 
+function getBookmarkBarId(node_array) {
+	var bookmarkBarId = null;
+	for (i = 0; i < node_array.length; i++) {
+		if (node_array[i].title === "Bookmarks Bar") {
+			bookmarkBarId = node_array[i].id;
+			break;
+		}
+		
+		/* recurse to keep parsing through the tree */
+		chrome.bookmarks.getChildren(node_array[i].id, getBookmarkBarId);
+	}
+	
+	if (bookmarkBarId != null) {
+		bookmarkFilteredTabs(bookmarkBarId);
+	}
+}
+
+function bookmarkFilteredTabs(bookmarkBarId_) {
+	var bookmarkParams = {
+		"parentId": bookmarkBarId_,
+		"title": "Tabulator Bookmarks"
+	};
+	
+	chrome.bookmarks.create(bookmarkParams);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("filter").addEventListener("keyup", updateTabList);
 	document.getElementById("onlyCurrentWindow").addEventListener("click", updateTabList);
+	document.getElementById("bookmarkButton").addEventListener("click",
+		function () {
+			chrome.bookmarks.getTree(getBookmarkBarId);
+		}
+	);
 	updateTabList();
 	document.getElementById("filter").focus();
 });
