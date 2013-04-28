c4da0ed76b697c00c55ad06984a772f315ccaa54
David Young <davidyoung604@gmail.com>
2013-04-27 19:14:28 -0700

    Functional version done.

--- popup.js
+++ popup.js
@@ -1,42 +1,54 @@
-function getTabs(tabs) {
-	for (i = 0; i < tabs.length; i++) {
-		var div = document.getElementById("windows");
-		div.innerHTML += tabs[i].url + "<br />";
+var filter = "";
+var windowId;
+var window_div = document.getElementById("windows");
+
+function windowHeader(num, win_id) {
+	if (win_id == windowId) {
+		isCurrent = " (current window)";
+	} else {
+		isCurrent = "";
 	}
+	
+	return "<h3>Window " + num + isCurrent + "</h3>";
 }
 
-function listTabs(windows) {
-	var div = document.getElementById("windows");
+function printTabs(tab_array) {
+	var re = new RegExp(filter, "gi");
 	
-	for (i = 0; i < windows.length; i++) {
-		var tabs = windows[i].tabs;
-		div.innerHTML += windowHeader(i);
-
-		for (j = 0; j < tabs.length; j++) {			
-			div.innerHTML += tabs[j].url + "<br />";
+	for (tab_index = 0; tab_index < tab_array.length; tab_index++) {
+		var url = tab_array[tab_index].url;
+		if ( re.test(url) ) {
+			window_div.innerHTML += url + "<br />";
 		}
 	}
 }
 
-function windowHeader(num) {
-	return "<h2>Window " + num + "</h2>";
+function listTabs(windows) {
+	for (win_index = 0; win_index < windows.length; win_index++) {
+		window_div.innerHTML += windowHeader(win_index, windows[win_index].id);
+		printTabs(windows[win_index].tabs);
+	}
+}
+
+function retrieveCurrentWindowId(window) {
+	windowId = window.id;
 }
 
 function updateTabList() {
 	document.getElementById("windows").innerHTML = "";
+	filter = document.getElementById("filter").value;
+	chrome.windows.getCurrent(retrieveCurrentWindowId);
+	
 	if (document.getElementById("onlyCurrentWindow").checked) {
-		chrome.windows.getCurrent(
-			function (window) {
-				chrome.tabs.getAllInWindow(window.id, getTabs);
-			}
-		);
+		chrome.tabs.getAllInWindow(windowId, printTabs);
 	} else {
 		chrome.windows.getAll( { "populate": true }, listTabs );
 	}
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("filter").addEventListener("keyup", updateTabList);
 	document.getElementById("onlyCurrentWindow").addEventListener("click", updateTabList);
 	updateTabList();
-	/* need to add filter logic */
+	document.getElementById("filter").focus();
 });
